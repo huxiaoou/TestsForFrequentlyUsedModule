d14daa90308accecf6758f1fdd02adcde6f5cfec
huxiaoou <516984451@qq.com>
2024-07-26 17:23:37 +0800

    Add complicated solutions of logging

--- main_logging.py
+++ main_logging.py
@@ -1,9 +1,12 @@
 if __name__ == "__main__":
     import logging
-    from module_logging import test_logging
+    from module_logging import test_logging, gen_custom_logger, test_custom_logger
 
-    test_logging(
-        filename=None,
-        filemode=None,
-        level=logging.INFO
-    )
+    # test_logging(
+    #     filename=None,
+    #     filemode=None,
+    #     level=logging.INFO
+    # )
+
+    logger = gen_custom_logger(filename="custom.log", filemode="a")
+    test_custom_logger(logger)
--- module_logging.py
+++ module_logging.py
@@ -6,7 +6,7 @@ def test_logging(filename: str | None, filemode: str | None, level: int = loggin
         filename=filename,
         filemode=filemode,
         level=level,
-        format="[%(levelname)8s][%(asctime)s.%(msecs)03d] %(message)s",
+        format="[%(levelname)-8s][%(asctime)s.%(msecs)03d] %(message)s",
         datefmt="%Y%m%d %H:%M:%S"
     )
 
@@ -15,3 +15,52 @@ def test_logging(filename: str | None, filemode: str | None, level: int = loggin
     logging.warning("This is log for WARNING")
     logging.error("This is log for ERROR")
     logging.critical("This is log for CRITICAL")
+
+
+def gen_custom_logger(filename: str | None, filemode: str | None) -> logging.Logger:
+    # logger = logging.getLogger()
+    # print(logger)
+
+    logger = logging.getLogger('huxo.applog')
+    # this overwrites handlers' level
+    # if this level is too high, then handlers' level won't work
+    # set logger's level to be lowest, a.k.a, logging.DEBUG, then handlers' level settings won't be affected
+    logger.setLevel(logging.DEBUG)
+    print(logger)
+
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.DEBUG)
+
+    file_handler = logging.FileHandler(filename=filename, mode=filemode)
+    file_handler.setLevel(logging.INFO)
+
+    formatter = logging.Formatter(
+        fmt="[%(levelname)-8s][%(asctime)s.%(msecs)03d] %(message)s",
+        datefmt="%Y%m%d %H:%M:%S",
+    )
+
+    console_handler.setFormatter(formatter)
+    file_handler.setFormatter(formatter)
+
+    logger.addHandler(console_handler)
+    logger.addHandler(file_handler)
+
+    flt = logging.Filter("huxo")  # can be added to handler
+    logger.addFilter(flt)  # logger will work only when logger.name start with "huxo"
+
+    return logger
+
+
+def test_custom_logger(logger: logging.Logger) -> None:
+    logger.debug("This is log for DEBUG")
+    logger.info("This is log for INFO")
+    logger.warning("This is log for WARNING")
+    logger.error("This is log for ERROR")
+    logger.critical("This is log for CRITICAL")
+
+    a = "abc"
+    try:
+        int(a)
+    except ValueError as e:
+        # logger.error(e)
+        logger.exception(e)
