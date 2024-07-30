c1434ebd569e143f8cea522870ef67b003477e60
huxiaoou <516984451@qq.com>
2024-07-30 15:54:23 +0800

    Add logger from library rich and loguru

--- main_logging.py
+++ main_logging.py
@@ -1,6 +1,9 @@
 if __name__ == "__main__":
     import logging
     from module_logging import test_logging, gen_custom_logger, test_custom_logger
+    from module_logging import get_rich_logger, test_rich_logger
+    from module_logging import get_custom_colorful_logger
+    from module_logging import test_for_loguru
 
     # test_logging(
     #     filename=None,
@@ -10,3 +13,11 @@ if __name__ == "__main__":
 
     logger = gen_custom_logger(filename="custom.log", filemode="a")
     test_custom_logger(logger)
+
+    rich_logger = get_rich_logger()
+    test_rich_logger(rich_logger)
+
+    logger = get_custom_colorful_logger()
+    test_custom_logger(logger)
+
+    test_for_loguru()
--- module_logging.py
+++ module_logging.py
@@ -1,4 +1,7 @@
+import sys
 import logging
+import rich.logging as rlog
+from loguru import logger as rlogger
 
 
 def test_logging(filename: str | None, filemode: str | None, level: int = logging.INFO) -> None:
@@ -64,3 +67,103 @@ def test_custom_logger(logger: logging.Logger) -> None:
     except ValueError as e:
         # logger.error(e)
         logger.exception(e)
+
+
+def get_rich_logger() -> logging.Logger:
+    formatter = logging.Formatter(
+        fmt="[%(levelname)-8s][%(asctime)s.%(msecs)03d] %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S"
+    )
+    rich_handler = rlog.RichHandler(
+        show_time=True, show_level=True, show_path=False,
+        markup=True,
+        # log_time_format="%Y-%m-%d %H:%M:%S.%f"
+    )
+    rich_handler.setFormatter(formatter)
+
+    logger = logging.getLogger('huxo.richlog')
+    logger.setLevel(logging.DEBUG)
+    logger.addHandler(rich_handler)
+
+    return logger
+
+
+def test_rich_logger(logger: logging.Logger) -> None:
+    logger.debug("This is log for [red]DEBUG[/red] aaa")
+    logger.info("This is log for [yellow]INFO bbb")
+    logger.warning("This is log for [green]WARNING[/green] ccc")
+    logger.error("This is log for [blue]ERROR[/blue] ddd")
+    logger.critical("This is log for [cyan]CRITICAL[/cyan] eee")
+
+    a = "abc"
+    try:
+        int(a)
+    except ValueError as e:
+        # logger.error(e)
+        logger.exception(e)
+
+
+class CustomFormatter(logging.Formatter):
+    grey = "\x1b[38;20m"
+    yellow = "\x1b[33;20m"
+    red = "\x1b[31;20m"
+    bold_red = "\x1b[31;1m"
+    reset = "\x1b[0m"
+    # format = "%(asctime)s - %(name)s - %(levelname)s - %(message)s (%(filename)s:%(lineno)d)"
+    format = "[%(levelname)-8s][%(asctime)s.%(msecs)03d] %(message)s"
+
+    FORMATS = {
+        logging.DEBUG: grey + format + reset,
+        logging.INFO: grey + format + reset,
+        logging.WARNING: yellow + format + reset,
+        logging.ERROR: red + format + reset,
+        logging.CRITICAL: bold_red + format + reset
+    }
+
+    def format(self, record: logging.LogRecord):
+        log_fmt = self.FORMATS.get(record.levelno)
+        # self._fmt = log_fmt
+        # return super().format(record)
+
+        formatter = logging.Formatter(log_fmt, datefmt="%Y-%m-%d %H:%M:%S")
+        return formatter.format(record)
+
+
+def get_custom_colorful_logger() -> logging.Logger:
+    logger = logging.getLogger("myColorfulApp")
+    logger.setLevel(logging.DEBUG)
+
+    # create console handler with a higher log level
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.DEBUG)
+
+    ch.setFormatter(CustomFormatter(datefmt="%Y-%m-%d %H:%M:%S"))
+
+    logger.addHandler(ch)
+    return logger
+
+
+def test_for_loguru() -> None:
+    rlogger.remove(0)
+    fmt_datetime = "[<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green>]"
+    fmt_level = "[<level>{level:<8}</level>]"
+    fmt_name = "<cyan>{name}</cyan>"
+    fmt_func = "<cyan>{function}</cyan>"
+    fmt_line = "<cyan>{line}</cyan>"
+    fmt_location = f"[{fmt_name}:{fmt_func}:{fmt_line}]"
+    # fmt_msg = "[<level>{message}</level>]"
+    fmt_msg = "[{message}]"
+
+    rlogger.add(
+        sys.stdout,
+        level="TRACE",
+        format=fmt_datetime + fmt_level + fmt_location + fmt_msg,
+    )
+
+    rlogger.trace("Executing program")
+    rlogger.debug("Processing data...")
+    rlogger.info("Server started successfully.")
+    rlogger.success("Data processing completed successfully.")
+    rlogger.warning("Invalid configuration detected.")
+    rlogger.error("Failed to connect to the database.")
+    rlogger.critical("Unexpected system error occurred. Shutting down.")
